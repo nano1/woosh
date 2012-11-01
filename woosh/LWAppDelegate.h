21919967a36b72166d78a22fd9f17494f669f28d
Steven L <steven373@gmail.com>
2012-10-31 20:47:19 -0700

    initial commit

---
+++ woosh/LWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LWAppDelegate.h
+//  woosh
+//
+//  Created by Steven Le on 10/31/12.
+//  Copyright (c) 2012 Steven Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ woosh/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  woosh
+//
+//  Created by Steven Le on 10/31/12.
+//  Copyright (c) 2012 Steven Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LWAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LWAppDelegate class]));
+    }
+}
